facf3c417d98d2e8f931232b72068a21ce76e440
Tisks <leiser.mahu@usach.cl>
2021-01-20 11:26:08 -0300

    Allowing any port to communicate (to be changed in the future)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http, {
     cors: {
-      origin: "http://172.31.37.79:8080",
+      origin: "*",
       methods: ["GET", "POST"]
     }
 });
