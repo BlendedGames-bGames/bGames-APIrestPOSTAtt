99b36a55b51a496faaab8ef88feccff0aa1a1d97
Tisks <leiser.mahu@usach.cl>
2021-02-20 23:32:29 -0300

    HOTFIX: io variable not defined

--- src/routes/real_time_attributes.js
+++ src/routes/real_time_attributes.js
@@ -120,6 +120,7 @@ real_time_attributes.post('/adquired_subattribute_rt', (req,res,next)=>{
 });
 
 real_time_attributes.post('/spent_attribute_rt', (req,res,next)=>{
+    const io = req.app.locals.io
 
     var spent_attribute = req.body;
     var id_player = spent_attribute.id_player
